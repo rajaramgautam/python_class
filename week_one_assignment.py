33b4ae3937bc4325d75828c032d670d558c9b75a
Rajaram Gautam <rajaram.gautam15@gmail.com>
2021-11-14 11:59:20 -0600

    First Week Assignment

---
+++ week_one_assignment.py
@@ -0,0 +1,57 @@
+# Instructions
+# Class,
+
+# You need to confirm the flowing:-
+
+# 1) Installation
+
+# To install Python, go to PythonDownload
+
+# select version 3.x, and follow the instructions
+
+# 2) I will not accept any code in Python interactive mode. To create a program in batch mode;
+
+# In IDLE
+
+# Go to File ->New File-> A new editor for coding will open, write your code in the new window.
+# Once you are done coding; hit F5, or go to run->Run module
+# If the file is not saved, it will prompt you to save the file. Make sure you know where you saved the file.
+# If the code has bugs, go back to the editor and fix the bugs, pay attention to the error message.
+# The final code is the code you should submit.
+# Write the following code
+"""
+
+Program name:
+
+Author: Rajaram Gautam
+
+Data: Week I Assignment
+
+Notes:
+
+"""
+
+# This is comment line
+
+# This is test to see if Python is working.
+
+
+myname="Rajaram"
+
+print("Hello ENTD220, My name is ", myname)
+
+# 3) MAC/Apple user
+
+# Check the uploaded files, and make sure they are readable.
+
+# **** Start working on WEEK2 assignment *****
+
+# Popular IDE;
+
+# I recommend downloading one of the following very popular IDE
+
+# 1) PyScripter; https://github.com/pyscripter/pyscripter -> downloads
+
+# 2) VS Code; https://code.visualstudio.com/Download;
+
+# This IDE support many languages, after installation install the desired language(s) extension.
